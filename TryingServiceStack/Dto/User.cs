592419387d8e094bb25087fc9e162399ee4b16e4
Alexey Zharov <to.alex.zharov@gmail.com>
2016-09-01 13:18:29 +0300

    Added IReturn and Swagger decoration

--- TryingServiceStack/Dto/User.cs
+++ TryingServiceStack/Dto/User.cs
@@ -1,8 +1,20 @@
-﻿namespace TryingServiceStack.Dto
+﻿using ServiceStack;
+
+namespace TryingServiceStack.Dto
 {
-    public class User
+    [Route("/user", Summary = "Hello Service", Notes = "More description about Hello Service")]
+    public class User : IReturn<UserResponse>
     {
+        /// <summary>
+        /// Profile id
+        /// </summary>
+        [ApiMember(Name = nameof(ProfileId), Description = "Profile id", ParameterType = "query", DataType = "string", IsRequired = false)]
         public string ProfileId { get; set; }
+
+        /// <summary>
+        /// Phone number
+        /// </summary>
+        [ApiMember(Name = nameof(Msisdn), Description = "Phone number", ParameterType = "query", DataType = "string", IsRequired = false)]
         public string Msisdn { get; set; }
     }
 }
