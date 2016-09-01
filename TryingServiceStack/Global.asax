51ae893f06b19b7cb310559b4d18e9430610d9ff
Alexey Zharov <to.alex.zharov@gmail.com>
2016-09-01 14:10:39 +0300

    Reinstalled Nuget packages

--- TryingServiceStack/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TryingServiceStack.Global" Language="C#" %>
--- TryingServiceStack/Properties/AssemblyInfo.cs
+++ TryingServiceStack/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("17436fc6-f110-413f-9632-bbe8acca29a1")]
+[assembly: Guid("263b0486-b069-4bca-bb77-e2ccc66c2bae")]
 
 // Version information for an assembly consists of the following four values:
 //
